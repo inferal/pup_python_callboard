c8fee7fe96463d532334c8d4a67d66741784ec10
Yura Pogorelov <pyurik44@gmail.com>
2019-06-10 22:00:45 +0300

    Фото
    Галерея

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+    """Галерея"""
+    name = models.CharField("Имя" , max_length=50)
+    photos = models.ManyToManyField(verbose_name="Фотографии")
+    slug = models.SlugField("url", max_length=50, unique=True)
