30777b8d8209e17cf4582406df87fb1637e14e6b
Yura Pogorelov <pyurik44@gmail.com>
2019-06-27 16:29:41 +0300

    Доработка Галлереи

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -1,17 +1,43 @@
-from django.db import models
+import os
 
-# Create your models here.
+from PIL import Image
+from django.db import models
+from datetime import datetime, date
+
+from django.utils import timezone
+
+
+def get_path_upload_image(file):
+    time = timezone.now().strftime("%Y-%m-%d")
+    end_extention = file.split('.')[1]
+    head = file.split('.')[0]
+    if len(head) > 10:
+        head = head[:10]
+    file_name = head + '_' + timezone.now().strftime("%h-%m-%S") + '.' + end_extention
+    return os.path.join('photos', '{}', '{}').format(time, file_name)
+
+
 class Photo(models.Model):
     """Фото"""
     name = models.CharField("Имя", max_length=50)
     # TODO: для image генерить путь (user, date)
-    # TODO: создовать миниатюры, ограничить вес фото и размер
-    image =models.ImageField("Фото", upload_to="gallery/")
+    image = models.ImageField("Фото", upload_to="gallery/")
     created = models.DateTimeField("Дата создания", auto_now_add=True)
     slug = models.SlugField("url", max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.image.name = get_path_upload_image(self.image.name)
+        super().save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 200 or img.width > 200:
+                output_size = (200, 200)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
     class Meta:
         verbose_name = "Изображение"
@@ -20,7 +46,7 @@ class Photo(models.Model):
 
 class Gallery(models.Model):
     """Галерея"""
-    name = models.CharField("Имя" , max_length=50)
+    name = models.CharField("Имя", max_length=50)
     photos = models.ManyToManyField(Photo, verbose_name="Фотографии")
     created = models.DateTimeField("Дата создания", auto_now_add=True)
     slug = models.SlugField("url", max_length=50, unique=True)
